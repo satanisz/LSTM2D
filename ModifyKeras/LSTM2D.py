a59aab418e5695a466186ae81735673572d3df4a
stanisz <przemyslaw.stanisz@arcelormittal.com>
2021-10-21 09:53:47 +0200

    init local commit

---
+++ ModifyKeras/LSTM2D.py
@@ -0,0 +1,117 @@
+import numpy as np
+from keras import backend as K
+from keras import Input, Model
+from keras.layers.recurrent import _generate_dropout_mask, LSTMCell, RNN
+import keras
+import tensorflow as tf
+
+class LSTM2D(LSTMCell):
+    def call(self, inputs, states, training=None):
+        if 0 < self.dropout < 1 and self._dropout_mask is None:
+            self._dropout_mask = _generate_dropout_mask(
+                K.ones_like(inputs),
+                self.dropout,
+                training=training,
+                count=4)
+        if (0 < self.recurrent_dropout < 1 and
+                self._recurrent_dropout_mask is None):
+            self._recurrent_dropout_mask = _generate_dropout_mask(
+                K.ones_like(states[0]),
+                self.recurrent_dropout,
+                training=training,
+                count=4)
+
+            # dropout matrices for input units
+        dp_mask = self._dropout_mask
+        # dropout matrices for recurrent units
+        rec_dp_mask = self._recurrent_dropout_mask
+
+        h_tm1 = states[0]  # previous memory state
+        c_tm1 = states[1]  # previous carry state
+
+        if self.implementation == 1:
+            print("Implementation: ", self.implementation)
+            if 0 < self.dropout < 1.:
+                inputs_i = inputs * dp_mask[0]
+                inputs_f = inputs * dp_mask[1]
+                inputs_c = inputs * dp_mask[2]
+                inputs_o = inputs * dp_mask[3]
+            else:
+                inputs_i = inputs
+                inputs_f = inputs
+                inputs_c = inputs
+                inputs_o = inputs
+            x_i = K.dot(inputs_i, self.kernel_i)
+            x_f = K.dot(inputs_f, self.kernel_f)
+            x_c = K.dot(inputs_c, self.kernel_c)
+            x_o = K.dot(inputs_o, self.kernel_o)
+            if self.use_bias:
+                x_i = K.bias_add(x_i, self.bias_i)
+                x_f = K.bias_add(x_f, self.bias_f)
+                x_c = K.bias_add(x_c, self.bias_c)
+                x_o = K.bias_add(x_o, self.bias_o)
+
+            if 0 < self.recurrent_dropout < 1.:
+                h_tm1_i = h_tm1 * rec_dp_mask[0]
+                h_tm1_f = h_tm1 * rec_dp_mask[1]
+                h_tm1_c = h_tm1 * rec_dp_mask[2]
+                h_tm1_o = h_tm1 * rec_dp_mask[3]
+            else:
+                h_tm1_i = h_tm1
+                h_tm1_f = h_tm1
+                h_tm1_c = h_tm1
+                h_tm1_o = h_tm1
+            i = self.recurrent_activation(x_i + K.dot(h_tm1_i,
+                                                      self.recurrent_kernel_i))
+            f = self.recurrent_activation(x_f + K.dot(h_tm1_f,
+                                                      self.recurrent_kernel_f))
+            c = f * c_tm1 + i * self.activation(x_c + K.dot(h_tm1_c,
+                                                            self.recurrent_kernel_c))
+            o = self.recurrent_activation(x_o + K.dot(h_tm1_o,
+                                                      self.recurrent_kernel_o))
+        else:
+            print("Implementation: ", self.implementation)
+            if 0. < self.dropout < 1.:
+                inputs *= dp_mask[0]
+            z = K.dot(inputs, self.kernel)
+            if 0. < self.recurrent_dropout < 1.:
+                h_tm1 *= rec_dp_mask[0]
+            z += K.dot(h_tm1, self.recurrent_kernel)
+            if self.use_bias:
+                z = K.bias_add(z, self.bias)
+
+            z0 = z[:, :self.units]
+            z1 = z[:, self.units: 2 * self.units]
+            z2 = z[:, 2 * self.units: 3 * self.units]
+            z3 = z[:, 3 * self.units:]
+
+            i = self.recurrent_activation(z0)
+            f = self.recurrent_activation(z1)
+            c = f * c_tm1 + i * self.activation(z2)
+            o = self.recurrent_activation(z3)
+
+        h = o * self.activation(c)
+        if 0 < self.dropout + self.recurrent_dropout:
+            if training is None:
+                h._uses_learning_phase = True
+        # return tf.expand_dims(tf.concat([h,c],axis=0),0), [h, c]
+        return h, [h, c]
+
+if __name__ == '__main__':
+    # create a cell
+    test = LSTM2D(100)
+
+    # Input timesteps=10, features=7
+    in1 = Input(shape=(10, 7))
+    out1 = RNN(test, return_sequences=True)(in1)
+
+    M = Model(inputs=[in1], outputs=[out1])
+    M.compile(keras.optimizers.Adam(), loss='mse')
+
+    ans = M.predict(np.arange(7 * 10, dtype=np.float32).reshape(1, 10, 7))
+
+    print(ans.shape)
+    # state_h
+    print(ans[0, 0, 0, :])
+    # state_c
+    print(ans[0, 0, 1, :])
